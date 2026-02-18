bcd116d4cff321f7ff5fe6fccd1b9c9cec04716a
pree-gupta <136050825+pree-gupta@users.noreply.github.com>
2026-02-18 15:58:16 +0530

    Fix import path for ConversationalRetrievalChain

--- tutor/chain.py
+++ tutor/chain.py
@@ -1,5 +1,5 @@
 from langchain_core.prompts import PromptTemplate
-from langchain-community.chains import ConversationalRetrievalChain
+from langchain_community.chains import ConversationalRetrievalChain
 
 from tutor.llm import llm
 from tutor.rag import retriever
